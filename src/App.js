0cb57034b72627cb2828de5e5682f777bc859e53
Rasul <rasuldevv@gmail.com>
2024-03-16 12:30:20 +0500

    c1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
 
-function App() {
+const App = () => {
+  const [data, setData] = useState({})
+
+  const handleInput = event => {
+    let newInput = {[event.target.name]: event.target.value}
+    setData({...data, ...newInput})
+  }
+  const handleSubmit = () => {
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input onChange={e => handleInput(e)} name="email" placeholder="email..." />
+      <input onChange={e => handleInput(e)} name="password" placeholder="password..." />
+      <button >submit</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/firebaseConfig.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDi-eMZ4mbfNK3Lxr8bATgl47voQbgV7Zo",
+  authDomain: "fir-frontend-6e8ed.firebaseapp.com",
+  projectId: "fir-frontend-6e8ed",
+  storageBucket: "fir-frontend-6e8ed.appspot.com",
+  messagingSenderId: "582522471747",
+  appId: "1:582522471747:web:068eaaa511a3b0bbdd1bf6"
+};
+
+export const app = initializeApp(firebaseConfig);
